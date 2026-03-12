75bdd57d068e96b8f1a82706def0f1aa691c5b0f
Andy Cooper <andy@smashedcrabstudio.com>
2026-03-12 12:09:41 +0000

    clean up dual project running

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,11 @@ import { AppProviders } from './app/providers/AppProviders'
 import { RouteContainer } from './app/routing/RouteContainer'
 
 function App() {
-  return (
-    <AppProviders>
-      <RouteContainer />
-    </AppProviders>
-  )
+    return (
+        <AppProviders>
+            <RouteContainer />
+        </AppProviders>
+    )
 }
 
 export default App
